a64a816618c597ad92c829fa49217339f8e3d8bd
W-A-S-A-Y <abdul.wasay@aiesec.net>
2021-04-30 23:37:05 +0500

    Sad nibba hours

--- something.cs
+++ something.cs
@@ -4,5 +4,6 @@ class MyClass
     MyClass() {
         Console.writeline("ahh this won't work");
         System.Console.WriteLine("Dead");
+        System.Console.WriteLine("I'm really a noob go easy on me.");
     }
 }
